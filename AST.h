c05a602092028478d48ab20c53c825485e31364f
Rexar <rexar.fr@gmail.com>
2021-04-28 12:46:33 +0200

    general functions

--- AST.h
+++ AST.h
@@ -3,33 +3,39 @@
 
 /* unary-and-binary tree structure */
 struct _tree {
-  char* car;                    	/* char for arithmetic operation */
-  int val;			/* int  for value */
-  struct _tree* childs;    	/* used for unary node but NULL if leaf */
-  struct _tree* next;   	/* NULL if unary node or leaf*/
+  char* car;           
+  int boolean_value;
+  char* var_name; 
+  double number_value; 
+  struct _tree* childs;    	
+  struct _tree* next;  
 };
 
 typedef struct _tree* AST;
 
-/* create an AST from a root value and two AST sons */
-AST newBinaryAST(char* car, AST left, AST right);
+AST newQuaternary AST(char *car, AST node1, AST node2 , AST node3 , AST node4);
+
+AST newTernaryAST(char *car, AST node1, AST node2 , AST node3 ); 
 
 /* create an AST from a root value and one AST son */
 AST newUnaryAST(char* car, AST son);
 
-/* create an AST leaf from a value */
-AST newLeafAST(int val);
-/* create an AST leaf from a value */
-AST newthirdelyAST(int val);
+/* create an AST from a root value and two AST sons */
+AST newBinaryAST(char* car, AST left, AST right);
+
+/* create an AST leaf from a number */
+AST newNumberLeafAST(double val);
+
+/* create an AST leaf from a boolean 0 for false 1 for true  */
+AST newBooleanLeafAST(int val);
+
+/* create an AST leaf from a variable */
+AST newVariableLeafAST(char* val);
+
 /* delete an AST */
 void freeAST(AST t);
 
 /* print an AST*/
 void printAST(AST t);
 
-AST newForASt( char* car , AST expression1 , AST expression2, AST expression3 , AST COMMAND );
-
-AST newWhile(char* car , AST command );
-
-AST ifelse(char * car , AST command1, AST command2);
 #endif // !_AST_H_

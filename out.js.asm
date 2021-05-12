7d20d84788c8647b42683eb927e1f837c601f07e
Rexar <rexar.fr@gmail.com>
2021-05-12 20:28:05 +0200

    fix var pointer

---
+++ out.js.asm
@@ -0,0 +1,19 @@
+CsteNb 1.000000
+CsteNb 2.000000
+AddiNb
+SetVar 
+CsteNb 1.000000
+CsteNb 0.020000
+AddiNb
+SetVar x = 1 + 2.e-2; 
+1 + 10 * 1.1e10 * x + y; 
+CsteNb 1.000000
+CsteNb 10.000000
+CsteNb 11000000000.000000
+MultNb
+GetVar x + y; 
+MultNb
+AddiNb
+GetVar y; 
+AddiNb
+Halt
